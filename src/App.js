57ce5bb9206446dbc3abcf2700ba944c21265933
Quinn <74791898+griffique@users.noreply.github.com>
2021-04-09 13:59:18 -0400

    Styled background color and font colors

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ function App() {
           <h3>What word do you want to look up?</h3>
         </header>
         <Dictionary />
+        <footer className="footer">
+          {" "}
+          <p>
+            {" "}
+            <a href="https://github.com/griffique"> Open-Source Code</a> by{" "}
+            <a href="https://nifty-wilson-61230a.netlify.app/">Quinn Griffin</a>{" "}
+            🐱‍🚀
+          </p>
+        </footer>
       </div>
     </div>
   );
