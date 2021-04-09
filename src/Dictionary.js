77460daf125bc4b31d3401f95fcacbd5c68bbbdc
Quinn <74791898+griffique@users.noreply.github.com>
2021-04-09 13:37:48 -0400

    created search form with useState and test alert of value

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="dictionary">
       {" "}
-      <form>
+      <form onSubmit={search}>
         {" "}
-        <input type="search" />{" "}
+        <input
+          type="search"
+          placeholder="Type a word here"
+          onChange={handleKeywordChange}
+        />{" "}
         <input type="submit" className="btn-success submit-button" />{" "}
       </form>
     </div>
