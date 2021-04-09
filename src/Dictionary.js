608893ddb51d9194aa78e0940e9e6e0e5461b9bc
Quinn <74791898+griffique@users.noreply.github.com>
2021-04-09 14:16:46 -0400

    added api call and console logged response

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
