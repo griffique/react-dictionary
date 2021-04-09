ab2f0e24eb0b3d1ae38d24a505361899f69852dd
Quinn <74791898+griffique@users.noreply.github.com>
2021-04-09 11:40:44 -0400

    updated emoji

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>📚 Dictionary App </h1>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <h1>📖 Dictionary App </h1>
 
-        <h3>What word do you want to look up?</h3>
-      </header>
+          <h3>What word do you want to look up?</h3>
+        </header>
+      </div>
     </div>
   );
 }
