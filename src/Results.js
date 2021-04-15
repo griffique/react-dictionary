7fa42bdba7b29651f7ed6504d062b8ab613152ab
Quinn <74791898+griffique@users.noreply.github.com>
2021-04-15 13:12:30 -0400

    Added loop to show multiple parts of speech and definitions.

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Results.css";
+import Meaning from "./Meaning.js";
 export default function Results(props) {
   if (props.results)
     return (
       <div className="results">
         {" "}
         <h2> Definition of {props.results.word}: </h2>
-        <p> {props.results.meanings[0].definitions[0].definition}</p>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
       </div>
     );
   else return null;
