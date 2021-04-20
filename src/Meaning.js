dbf5315963f2c8cbec709d560b8cface11f9b87a
Quinn <74791898+griffique@users.noreply.github.com>
2021-04-20 11:09:34 -0400

    Added phonetic text and audio link

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,7 +4,7 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <h2>{props.meaning.partOfSpeech}</h2>
       {[
         props.meaning.definitions.map(function (definition, index) {
           return (
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,4 +1,9 @@
 import React from "react";
 export default function Phonetic(props) {
-  return <div className="phonetics">test</div>;
+  return (
+    <div className="phonetics">
+      <a href={props.phonetic.audio}>Listen</a>
+      <h3>{props.phonetic.text}</h3>
+    </div>
+  );
 }
